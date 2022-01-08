7f840f9d6ccd2b676d16462bdce917bc5b9d0c11
Mohammad Abdul Imad <abdimad786@gmail.com>
2022-01-09 01:19:05 +0530

    some bug fixes

--- src/Components/nav.js
+++ src/Components/nav.js
@@ -35,8 +35,8 @@ const NavBar = () => {
 				userId: auth.currentUser.uid,
 				posturl: url,
 				uploadedAt: Timestamp.fromDate(new Date()),
-				likes: [''],
-				comments: [''],
+				likes: [],
+				comments: [],
 				username: currentUserData.username,
 				profilePicture: 'www.xyz.com',
 				// comments:[{comment:"Comment",id:auth.currentUser.uid}]
@@ -230,7 +230,7 @@ const NavBar = () => {
 							setIsOpen(!isOpen);
 						}}
 					/>
-					<div class='dropdown-content' id='dropdown_uniq'>
+					<div className='dropdown-content' id='dropdown_uniq'>
 						<Link to={'/my-profile'}>My Profile</Link>
 						<Link to={'/edit-profile'}>Edit Profile</Link>
 						<a href='logout' onClick={Logout}>
